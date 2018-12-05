a00c2f73ef39109235f9b0c5c4277e653d77acb2
chrfarri <chrfarri@imail.iu.edu>
2018-12-05 17:45:27 -0500

    Add files via upload

--- src/Progression_Modeling.py
+++ src/Progression_Modeling.py
@@ -10,43 +10,105 @@ from tkinter.filedialog import askopenfilename
 from sklearn.preprocessing import normalize
 from librosa.feature import melspectrogram
 from librosa import cqt
+from librosa import pseudo_cqt
 from librosa.effects import trim
- #Define notes on Piano
+
+#Define notes on Piano
 A0 = 27.5
+A1 = 55.00
+C1 = 32.70
+A2 = 110.00
 C2 = 65.41
+E6 = 1318.51
 C7 = 2093.00
 C8 = 4186.01
- #Loads data from .wav file specified
+
+#Loads data from .wav file specified
 def Load():
     filename=askopenfilename()
     data, sr = sf.read(filename)
-     return data, sr
- #Given the .wav data, cuts stereo if needed and trims silence
+
+    return data, sr
+
 def Prepare(data):
     T = data.T
     if np.shape(T)[0]==2:
         data = T[0]
-     data = trim(data, 40)[0]
-     return data
- #Once data is Prepared, input it with the sr to return Mel_Transformed spectrogram
-#If feeding into a NN for a single chord take the average frame of this
+
+    data = trim(data, 30)[0]
+
+    return data
+
+
 def Mel_transform(data, sr):
-    Mel = melspectrogram(data, sr, fmin=C2, fmax=C7, n_mels=100, n_fft=2048*2, hop_length=1024)
+    Mel = melspectrogram(data, sr, fmin=10, fmax=E6, n_mels=100, n_fft=2048*3, hop_length=512*3)
     N_Mel = normalize(Mel, norm='l1', axis=0)
     return pow(N_Mel, .25)
+
+def CQT_transform(data, sr):
+    CQT = CQT = abs(pseudo_cqt(data, sr, fmin=A1, n_bins=120, bins_per_octave=24, sparsity=.95, window=('kaiser', 10)))
+    N_CQT = normalize(CQT, norm='l1', axis=0)
+    return N_CQT
     
-#Grabs data, prepares, and transforms in one function
+
 def Get_Mel():
     data, sr = Load()
     data = Prepare(data)
     Mel = Mel_transform(data, sr)
     return Mel
- #Given number of classes and a classified sequence from GMM,
+
+def Get_CQT():
+    data, sr = Load()
+    data = Prepare(data)
+    CQT = CQT_transform(data, sr)
+    return CQT
+
+def Get_Boundaries(Progression):
+    N = len(Progression)
+    Boundaries = list()
+    i = 0
+    j = 0
+    state = Progression[i]
+    while j<N-1:
+        j += 1
+        next_state = Progression[j]
+        if next_state != state or j==N-1:
+            if j-i >= 10:
+                Boundaries.append([i, j])
+                
+            i = j
+            state = Progression[i]
+            
+    return Boundaries
+            
+            
+        
+
+def Deaden(M, tau):
+    R, C = np.shape(M)
+    M_copy = np.copy(M)
+    for i in range(R):
+        for j in range(C):
+            if M_copy[i][j] < tau:
+                M_copy[i][j] = 0
+    return M_copy
+
+
+#Given number of classes and a classified sequence,
 #Creates an image to plot
 def Plot_Prog(k, Prog):
     N = len(Prog)
     S = np.zeros((k, N))
     for i in range(N):
         S[Prog[i]][i] = 1
-     plt.imshow(S, aspect=10)
+
+    plt.imshow(S, aspect=10)
     plt.show()
+
+def Project_Prog(k, Prog):
+    N = len(Prog)
+    S = np.zeros((k, N))
+    for i in range(N):
+        S[Prog[i]][i] = 1
+
+    return S
